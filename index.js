0e05639df859e23171d6945ba7addc1f7b27646f
Arnoud5 <rey.sarayar@gmail.com>
2022-10-02 16:23:31 +0700

    add some routing

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 const http = require("http");
 const fs = require("fs");
+const port = 8080;
 
-http.createServer((req, res) => {
-    console.log(`Server is running at port 8080`);
+const server = http.createServer((req, res) => {
     if (req.url == "/") {
         fs.readFile("index.html", (err, data) => {
             res.writeHead(200, { "Content-Type": "text/html", "Content-Length": data.length });
             res.write(data);
             res.end();
         });
-    } else if (req.url == "/about.html"){
+    } else if (req.url == "/about.html") {
         fs.readFile("about.html", (err, data) => {
             res.writeHead(200, { "Content-Type": "text/html", "Content-Length": data.length });
             res.write(data);
             res.end();
         });
-    } else if (req.url == "/contact-me.html"){
+    } else if (req.url == "/contact-me.html") {
         fs.readFile("contact-me.html", (err, data) => {
             res.writeHead(200, { "Content-Type": "text/html", "Content-Length": data.length });
             res.write(data);
@@ -28,4 +28,8 @@ http.createServer((req, res) => {
             res.end();
         });
     }
-}).listen(8080);
+});
+
+server.listen(port, () => {
+    console.log(`Your server run at ${port}`)
+});
